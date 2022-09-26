50e1dca8f0b90b1fcf1ca6a1136318f31b4cb2d9
MatheusGuion <81374933+MatheusGuion@users.noreply.github.com>
2022-09-26 11:23:17 -0300

    Update DFPlayer.ino

--- Arcade/Musica_Arcade/DFPlayer.ino
+++ Arcade/Musica_Arcade/DFPlayer.ino
@@ -9,7 +9,7 @@ void printDetail(uint8_t type, int value);
 
 void setup() 
 {
-  mySoftawareSerial.begin(9600);
+  mySoftwareSerial.begin(9600);
   Serial.begin(115200);
 
   Serial.println();
@@ -19,26 +19,30 @@ void setup()
   if (!myDFPlayer.begin(mySoftwareSerial)) {
     Serial.println(F("Unable to begin"));
     Serial.println(F("1.Please recheck the connection!"));
-    Serial.println(F("2.Please insert de SD card!")):
+    Serial.println(F("2.Please insert de SD card!"));
     while(true);
   }
   Serial.println(F("DFPlayer Mini online."));
 
-  myDFPlayer.setTimeout(500);
+  myDFPlayer.setTimeOut(500);
 
   myDFPlayer.volume(10);
-  myDFPlayer.volumeUP();
+  myDFPlayer.volumeUp();
   myDFPlayer.volumeDown();
 
   myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
 
-  myDFPlayer.outputdevice(DFPLAYER_DEVICE_SD);
+  myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
 
   myDFPlayer.play(1);
 
   Serial.println(myDFPlayer.readState());
-  Serial.println(myDFPlayer.readeVolume());
+  Serial.println(myDFPlayer.readVolume());
   Serial.println(myDFPlayer.readEQ());
   Serial.println(myDFPlayer.readFileCounts());
   Serial.println(myDFPlayer.readCurrentFileNumber());
   Serial.println(myDFPlayer.readFileCountsInFolder(3));
+}
+void loop(){
+
+}
