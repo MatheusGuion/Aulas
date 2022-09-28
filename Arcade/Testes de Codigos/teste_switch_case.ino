0e402051f4adb6831255cc378675544514243e9c
MatheusGuion <81374933+MatheusGuion@users.noreply.github.com>
2022-09-28 09:32:11 -0300

    Add files via upload

---
+++ Arcade/Testes de Codigos/teste_switch_case.ino
@@ -0,0 +1,45 @@
+const int Button_A = 4;
+const int Button_B = 5;
+const int Button_C = 7; 
+const int Button_D = 8;
+const int DFPlayer = (10,11); 
+void setup();{
+  Serial.begin(9600);
+  pinMode(Button_A, INPUT);
+  pinMode(Button_B, INPUT);
+  pinMode(Button_C, INPUT);
+  pinMode(Button_D, INPUT);
+}
+
+void loop(){
+  if (Serial.available()){
+  char Button = Serial.read();
+  switch(Button){  
+  case '1':
+  digitalWrite(Button_A, HIGH);
+  digitalWrite(Button_B, LOW);
+  digitalWrite(Button_C, LOW);
+  digitalWrite(Button_D, LOW);
+  break;
+
+  case '2';
+  digitalWrite(Button_A, LOW);
+  digitalWrite(Button_B, HIGH);
+  digitalWrite(Button_C, LOW);
+  digitalWrite(Button_D, LOW);
+  break;
+
+  case '3';
+  digitalWrite(Button_A, LOW);
+  digitalWrite(Button_B, LOW);
+  digitalWrite(Button_C, HIGH);
+  digitalWrite(Button_D, LOW);
+  break;
+
+  case'4';
+  digitalWrite(Button_A, LOW);
+  digitalWrite(Button_B, HIGH);
+  digitalWrite(Button_C, LOW);
+  digitalWrite(Button_D, LOW);
+  digitalWrite(DFPlayer, HIGH);
+}
